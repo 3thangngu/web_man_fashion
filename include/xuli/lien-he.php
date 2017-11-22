1a08e31a770a8201ad299305f9ca360df89377aa
thanhle <lengoctienthanh@gmail.com>
2017-11-22 11:04:08 +0700

    upsidebar

---
+++ include/xuli/lien-he.php
@@ -0,0 +1,27 @@
+<?php
+	include('../inc/myconnect.php');
+	include('../inc/function.php');
+
+		if(isset($_GET['name']) && isset($_GET['email']) &&  isset($_GET['sdt']) && isset($_GET['address']) && isset( $_GET['content'])){
+			$name = $_GET['name'];
+			$email = $_GET['email'];
+			$sdt = $_GET['sdt'];
+			$address = $_GET['address'];
+			$content = $_GET['content'];
+
+			$query ="INSERT INTO tb_contact(
+											name,
+											email,
+											number_phone, 
+											address,
+											content
+										) VALUES(
+										'{$name}',
+										'{$email}', 
+										'{$sdt}', 
+										'{$address}', 
+										'{$content}'
+									)";
+			$result =  mysqli_query($dbc,$query);
+		}
+?>
